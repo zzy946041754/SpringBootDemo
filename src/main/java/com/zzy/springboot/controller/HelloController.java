ca135006960827beb6eb3bd5eef0b28ed94fa948
zzy <zzz946041754@163.com>
2019-09-05 18:30:10 +0800

    提交

--- src/main/java/com/zzy/springboot/controller/HelloController.java
+++ src/main/java/com/zzy/springboot/controller/HelloController.java
@@ -1,12 +1,19 @@
 package com.zzy.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.util.Date;
+
+@Controller
 public class HelloController {
     @RequestMapping("/hello")
-    public String hello(){
-            return "Hello Spring Boot!";
+    public String hello(HttpServletRequest request){
+        request.setAttribute("now",DateFormat.getDateTimeInstance().format(new Date()));
+
+            return "hello";
         }
 }
